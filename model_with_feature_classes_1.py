0ca5c8801d95e78c557f565386d38aa2f6543334
Marcel Mann <you@example.com>
2025-09-04 23:43:26 +0200

    commit

--- model_with_feature_classes_1.py
+++ model_with_feature_classes_1.py
@@ -37,14 +37,14 @@ class GELU(nn.Module):
 class FeedForward(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.layer_stack = nn.Sequential(
+        self.layers = nn.Sequential(
             nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
             GELU(),
             nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
         )
     
     def forward(self, x):
-        return self.layer_stack(x)
+        return self.layers(x)
 
 
 class MultiHeadAttention(nn.Module):
