18a4ebf8c688e499c39dd54915536631d6264ffc
Marcel Mann <you@example.com>
2025-09-09 18:57:22 +0200

    commit

--- helper_functions.py
+++ helper_functions.py
@@ -14,6 +14,27 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx = torch.cat((idx.to(device), idx_next.to(device)), dim=1)
     return idx
 
+def generate(model, idx, max_new_tokens, context_Size, top_k=5, temperature=1.0):
+    for _ in range(max_new_tokens):
+        idx_split = idx[:, -context_Size:]
+
+        with torch.no_grad():
+            logits = model(idx_split)
+
+        logits_last = logits[:, -1, :]
+
+        logits_last = logits_last / temperature
+
+
+        values, indices = torch.topk(logits_last, k=top_k, dim=-1)
+        probs = torch.softmax(values, dim=-1)
+        sampled_idx = torch.multinomial(probs, num_samples=1)
+        maxi = torch.gather(indices, -1, sampled_idx)
+
+
+        idx = torch.cat((idx, maxi), dim=1)
+
+    return idx
 
 def text_to_token_ids(text, tokenizer):
     encoded = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
